6b8bc84f076988b46232c3e709441501b7187fcc
Mohammad Ariful Islam <marifulislamdu@gmail.com>
2026-07-25 13:36:29 +0600

    Fix CSV paths and add costings data insertion
    Updated file paths for CSV data sources and added costings data insertion.

--- scripts/sql_scripts/inserting_data.sql
+++ scripts/sql_scripts/inserting_data.sql
@@ -1,28 +1,40 @@
+/*
+===========================================================================
+Insert Data to The Central Database.
+===========================================================================
+Script purpose: 
+        This script represents for inserting data from local csv file that we have made for this projects using python scripts.
+Note: 
+        The sources of the data comes from the 'erp' for this reason stored all data in 'erp' schema of the database.
+
+*/
+
+
 COPY erp.employees 
-FROM 'D:/data_scripts/employees.csv'
+FROM 'D:/data_scripts/data/employees.csv'
 WITH (
     FORMAT csv,
     HEADER true,
     DELIMITER ','
 );
-
 COPY erp.salaries 
-FROM 'D:/data_scripts/salaries.csv' 
+FROM 'D:/data_scripts/data/salaries.csv' 
 WITH (
     FORMAT csv,
     HEADER true,
     DELIMITER ','
 );
-
 COPY erp.products 
-FROM 'D:/data_scripts/products.csv'
+FROM 'D:/data_scripts/data/products.csv'
 WITH (
     FORMAT csv,
     HEADER true,
     DELIMITER ','
 );
-
-
-SELECT * FROM erp.employees;
-SELECT * FROM erp.salaries;
-SELECT * FROM erp.products;
+COPY erp.costings
+FROM 'D:/data_scripts/data/costings.csv'
+WITH (
+    FORMAT csv,
+    HEADER true,
+    DELIMITER ','
+);
